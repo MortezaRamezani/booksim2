e9ca91b2c166ca035ca0154ddb1beb9a2a3ef4df
Morteza Ramezani <morteza.r25@gmail.com>
2014-09-09 16:47:49 +0430

    It's not my fault :(

--- utils/trace_traffic_pattern.py
+++ utils/trace_traffic_pattern.py
@@ -12,10 +12,9 @@ for ln in fp:
         break
     line_temp = ln.split()
     line = map(int, line_temp)
-    pattern[line[1]*49+line[2]] += 1
-    
+    if( line[1] != line[2]):
+      pattern[line[1]*49 + line[2]] += 1
     
 for i in range(0, 49):
   for j in range(0, 49):
     print pattern[i*49 + j], 
-  print '\n'
